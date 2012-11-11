11fb6cb6aa0c57b6c6fb369b27c8e80bb96d0ec2
Steven Bethard <steven.bethard@gmail.com>
2012-11-11 17:35:12 +0100

    Specify FieldCache type for AgeIndex.

--- src/main/scala/info/bethard/litsearch/AgeIndex.scala
+++ src/main/scala/info/bethard/litsearch/AgeIndex.scala
@@ -7,11 +7,12 @@ import org.apache.lucene.queries.function.FunctionQuery
 import org.apache.lucene.queries.function.valuesource.IntFieldSource
 import org.apache.lucene.queries.function.valuesource.LinearFloatFunction
 import org.apache.lucene.queries.function.ValueSource
+import org.apache.lucene.search.FieldCache
 
 class AgeIndex(currentYear: Int, scoreAdjuster: (ValueSource => ValueSource)) extends Index {
 
   override def createQuery(queryText: String): Query = {
-    val yearSource = new IntFieldSource(IndexConfig.FieldNames.year)
+    val yearSource = new IntFieldSource(IndexConfig.FieldNames.year, FieldCache.NUMERIC_UTILS_INT_PARSER)
     val ageSource = new LinearFloatFunction(yearSource, -1f, currentYear.toFloat)
     new FunctionQuery(scoreAdjuster(ageSource))
   }
